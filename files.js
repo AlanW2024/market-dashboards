1f6a145361c0107f97b67157f41106cf931f7db5
AlanW2024 <alanwong23032023@gmail.com>
2026-03-18 21:45:35 +0800

    Sync dashboards 2026-03-18 21:45

--- files.js
+++ files.js
@@ -50,6 +50,7 @@ const FILES = {
     {"name":"FOXA_2026-03-18_投資分析框架.html","path":"TradingAgents/US/FOXA_2026-03-18_投資分析框架.html","dir":"TradingAgents/US"},
     {"name":"FOXA_2026-03-18_zh.html","path":"TradingAgents/US/FOXA_2026-03-18_zh.html","dir":"TradingAgents/US"},
     {"name":"FOXA_2026-03-18.html","path":"TradingAgents/US/FOXA_2026-03-18.html","dir":"TradingAgents/US"},
+    {"name":"GOOG_2026-03-18_投資分析框架.html","path":"TradingAgents/US/GOOG_2026-03-18_投資分析框架.html","dir":"TradingAgents/US"},
     {"name":"GOOG_2026-03-18_zh.html","path":"TradingAgents/US/GOOG_2026-03-18_zh.html","dir":"TradingAgents/US"},
     {"name":"GOOG_2026-03-18.html","path":"TradingAgents/US/GOOG_2026-03-18.html","dir":"TradingAgents/US"},
     {"name":"LULU_2026-03-18_zh.html","path":"TradingAgents/US/LULU_2026-03-18_zh.html","dir":"TradingAgents/US"},
