be539c825b4d3bcc25229d3689459b3523442051
AlanW2024 <alanwong23032023@gmail.com>
2026-03-18 21:47:47 +0800

    Sync dashboards 2026-03-18 21:47

--- files.js
+++ files.js
@@ -73,6 +73,7 @@ const FILES = {
     {"name":"TTD_2026-03-18_投資分析框架.html","path":"TradingAgents/US/TTD_2026-03-18_投資分析框架.html","dir":"TradingAgents/US"},
     {"name":"TTD_2026-03-18_zh.html","path":"TradingAgents/US/TTD_2026-03-18_zh.html","dir":"TradingAgents/US"},
     {"name":"TTD_2026-03-18.html","path":"TradingAgents/US/TTD_2026-03-18.html","dir":"TradingAgents/US"},
+    {"name":"VICI_2026-03-18_投資分析框架.html","path":"TradingAgents/US/VICI_2026-03-18_投資分析框架.html","dir":"TradingAgents/US"},
     {"name":"VICI_2026-03-18_zh.html","path":"TradingAgents/US/VICI_2026-03-18_zh.html","dir":"TradingAgents/US"},
     {"name":"VICI_2026-03-18.html","path":"TradingAgents/US/VICI_2026-03-18.html","dir":"TradingAgents/US"},
   ],
