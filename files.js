36b18930dc913950d2da9e5efc4202b04471bac6
AlanW2024 <alanwong23032023@gmail.com>
2026-03-18 15:47:08 +0800

    Sync dashboards 2026-03-18 15:47

--- files.js
+++ files.js
@@ -33,6 +33,7 @@ const FILES = {
     {"name":"DLR_2026-03-17.html","path":"TradingAgents/US/DLR_2026-03-17.html","dir":"TradingAgents/US"},
     {"name":"FDS_2026-03-18_zh.html","path":"TradingAgents/US/FDS_2026-03-18_zh.html","dir":"TradingAgents/US"},
     {"name":"FDS_2026-03-18.html","path":"TradingAgents/US/FDS_2026-03-18.html","dir":"TradingAgents/US"},
+    {"name":"FOX_2026-03-18_zh.html","path":"TradingAgents/US/FOX_2026-03-18_zh.html","dir":"TradingAgents/US"},
     {"name":"FOX_2026-03-18.html","path":"TradingAgents/US/FOX_2026-03-18.html","dir":"TradingAgents/US"},
     {"name":"FOXA_2026-03-18_zh.html","path":"TradingAgents/US/FOXA_2026-03-18_zh.html","dir":"TradingAgents/US"},
     {"name":"FOXA_2026-03-18.html","path":"TradingAgents/US/FOXA_2026-03-18.html","dir":"TradingAgents/US"},
