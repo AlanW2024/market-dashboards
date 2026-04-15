9a6c53ec0f73ab900320daf5ed3950407af2e04d
AlanW2024 <alanwong23032023@gmail.com>
2026-04-15 16:24:57 +0800

    Sync dashboards 2026-04-15 16:24

--- files.js
+++ files.js
@@ -1,5 +1,6 @@
 const FILES = {
   "🇺🇸 美股盤前": [
+    {"name":"2026-04-15 盤前Dashboard.html","path":"美股/盤前/2026-04-15 盤前Dashboard.html","dir":"美股/盤前"},
     {"name":"2026-04-14 盤前Dashboard.html","path":"美股/盤前/2026-04-14 盤前Dashboard.html","dir":"美股/盤前"},
     {"name":"2026-04-13 美股盤前Dashboard.html","path":"美股/盤前/2026-04-13 美股盤前Dashboard.html","dir":"美股/盤前"},
     {"name":"2026-04-10 盤前Dashboard.html","path":"美股/盤前/2026-04-10 盤前Dashboard.html","dir":"美股/盤前"},
