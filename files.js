cdcc669ec4bd01d36a7eb2ff3ee5e591ca10e1a2
AlanW2024 <alanwong23032023@gmail.com>
2026-05-05 15:04:10 +0800

    Sync dashboards 2026-05-05 15:04

--- files.js
+++ files.js
@@ -31,6 +31,7 @@ const FILES = {
     {"name":"2026-03-17 盤前Dashboard.html","path":"美股/盤前/2026-03/2026-03-17 盤前Dashboard.html","dir":"美股/盤前/2026-03","month":"2026-03"},
   ],
   "🇺🇸 美股盤後": [
+    {"name":"2026-05-04 美股盤後Dashboard.html","path":"美股/盤後/2026-05-04 美股盤後Dashboard.html","dir":"美股/盤後"},
     {"name":"2026-04-28 美股盤後Dashboard.html","path":"美股/盤後/2026-04-28 美股盤後Dashboard.html","dir":"美股/盤後"},
     {"name":"2026-04-23 美股盤後Dashboard.html","path":"美股/盤後/2026-04-23 美股盤後Dashboard.html","dir":"美股/盤後"},
     {"name":"2026-04-22 美股盤後Dashboard.html","path":"美股/盤後/2026-04-22 美股盤後Dashboard.html","dir":"美股/盤後"},
@@ -71,6 +72,7 @@ const FILES = {
     {"name":"2026-W18 美股週報Dashboard.html","path":"美股/週報/2026-05/2026-W18 美股週報Dashboard.html","dir":"美股/週報/2026-05","month":"2026-05"},
   ],
   "🇭🇰 港股盤前": [
+    {"name":"2026-05-05 港股盤前Dashboard.html","path":"港股/盤前/2026-05-05 港股盤前Dashboard.html","dir":"港股/盤前"},
     {"name":"2026-05-04 港股盤前Dashboard.html","path":"港股/盤前/2026-05-04 港股盤前Dashboard.html","dir":"港股/盤前"},
     {"name":"2026-04-24 港股盤前Dashboard.html","path":"港股/盤前/2026-04-24 港股盤前Dashboard.html","dir":"港股/盤前"},
     {"name":"2026-04-23 港股盤前Dashboard.html","path":"港股/盤前/2026-04-23 港股盤前Dashboard.html","dir":"港股/盤前"},
